352b042d11c612a6a6e4ca8c7f095b3b8d2b2962
shreyash-attryb <shreyash@attryb.com>
2023-11-14 12:13:13 +0530

    Bot image on projects

--- models/project.model.js
+++ models/project.model.js
@@ -3,7 +3,11 @@ const { default: mongoose } = require("mongoose");
 const projectSchema = mongoose.Schema({
   projectName: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-  botImage: { type: String, default: "" },
+  botImage: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdFQolqmgNtMjNW9kS-CApwChIYSIY_pdyjB0pYbk&s",
+  },
 });
 
 const projectModal = mongoose.model("project", projectSchema);
